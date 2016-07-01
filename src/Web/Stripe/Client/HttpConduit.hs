522942a0e774202f126a20e35ac320ed060dca08
Jason Whittle <jason.whittle@gmail.com>
2016-07-01 16:40:46 -0400

    A first attempt at implementation.

--- src/Web/Stripe/Client/HttpConduit.hs
+++ src/Web/Stripe/Client/HttpConduit.hs
@@ -1,8 +1,74 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Web.Stripe.Client.HttpConduit
-       ( callAPI
-       , withManager
+       ( stripe
+       , stripeMan
+       , StripeRequest (..)
+       , StripeError (..)
+       , StripeConfig (..)
+       , callAPI
        ) where
 
-callAPI = undefined
+import           Control.Monad.Trans.Resource (runResourceT)
+import           Data.Aeson.Parser (json)
+import           Data.Aeson.Types (FromJSON, fromJSON, Result, Value)
+import           Data.Conduit (($$+-))
+import           Data.Conduit.Attoparsec (ParseError(..), sinkParserEither)
+import           Data.Default (def)
+import           Data.List (intercalate)
+import           Data.Monoid ((<>))
+import           Data.Text.Encoding (encodeUtf8)
+import           Network.HTTP.Conduit (http, Manager, newManager, tlsManagerSettings)
+import qualified Network.HTTP.Conduit as H
+import           Network.HTTP.Types.Method
+import           Network.HTTP.Types.Status
+import           Web.Stripe.Client (APIVersion(..), attemptDecode, handleStream, parseFail, StripeConfig(..), StripeError(..), StripeKey(..), StripeRequest, StripeReturn, toBytestring, unknownCode)
+import qualified Web.Stripe.StripeRequest as S
 
-withManager = undefined
+stripe :: (FromJSON (StripeReturn a))
+       => StripeConfig
+       -> StripeRequest a
+       -> IO (Either StripeError (StripeReturn a))
+stripe config request = do
+  man <- newManager tlsManagerSettings
+  stripeMan man config request
+
+stripeMan :: (FromJSON (StripeReturn a))
+          => Manager
+          -> StripeConfig
+          -> StripeRequest a
+          -> IO (Either StripeError (StripeReturn a))
+stripeMan man config request = callAPI man fromJSON config request
+
+callAPI :: Manager
+        -> (Value -> Result b)
+        -> StripeConfig
+        -> StripeRequest a
+        -> IO (Either StripeError b)
+callAPI man fromJSON' config stripeRequest = runResourceT $ do
+  let req = H.applyBasicAuth (getStripeKey $ secretKey config) mempty $
+            def { H.method = (m2m $ S.method stripeRequest)
+                , H.secure = True
+                , H.host = "api.stripe.com"
+                , H.port = 443
+                , H.path = encodeUtf8 $ "/v1/" <> S.endpoint stripeRequest
+                , H.requestHeaders = [("Stripe-Version", toBytestring V20141007)]
+                , H.checkStatus = \_ _ _ -> Nothing
+                }
+  let req' = if S.GET == S.method stripeRequest
+             then H.setQueryString (map (\(f,s) -> (f, Just s)) $ S.queryParams stripeRequest) req
+             else H.urlEncodedBody (S.queryParams stripeRequest) req
+  res <- http req' man
+  let status = statusCode $ H.responseStatus res
+  if not $ attemptDecode status
+    then return unknownCode
+    else do v <- H.responseBody res $$+- sinkParserEither json
+            return $ case v of
+              (Left e) -> parseFail . intercalate " | " $ errorMessage e: errorContexts e
+              (Right a) -> handleStream fromJSON' status $ return a
+
+m2m :: S.Method -> Method
+m2m S.GET = methodGet
+m2m S.POST = methodPost
+m2m S.DELETE = methodDelete
--- tests/Main.hs
+++ tests/Main.hs
@@ -1,13 +1,24 @@
 module Main where
 
+import Control.Monad.Trans.Free (FreeF(..), FreeT(..))
+import Network.HTTP.Conduit (Manager, newManager, tlsManagerSettings)
 import           Web.Stripe.Client              (StripeConfig(..), StripeError(..))
-import           Web.Stripe.Client.HttpConduit  (callAPI, withManager)
+import           Web.Stripe.Client.HttpConduit  (callAPI)
 import           Web.Stripe.Test.AllTests       (allTests)
 import           Web.Stripe.Test.Prelude        (Stripe, StripeRequestF(..))
-import           Web.Stripe.Test.AllTests       (allTests)
 
 main :: IO ()
-main = allTests runStripe
+main = do
+  man <- newManager tlsManagerSettings
+  allTests (runStripe man)
 
-runStripe :: StripeConfig -> Stripe a -> IO (Either StripeError a)
-runStripe = undefined
+runStripe :: Manager -> StripeConfig -> Stripe a -> IO (Either StripeError a)
+runStripe man config (FreeT m) = do
+  f <- m
+  case f of
+   (Pure a) -> return (Right a)
+   (Free (StripeRequestF req decode')) -> do
+     r <- callAPI man decode' config req
+     case r of
+      (Left e) -> return (Left e)
+      (Right next) -> runStripe man config next
